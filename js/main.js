7aef95203bd0766360657b3cf2181813a9288e84
butkodenis <Butko.dv@gmail.com>
2017-05-12 20:18:13 +0300

    добавил выполнение js на нужных страницах

--- js/main.js
+++ js/main.js
@@ -60,72 +60,85 @@ jQuery(document).ready(function ($) {
 
 
         // изминение при прокрутке nav
-    $(window).on("scroll", function() {
-        if ($(window).scrollTop() > 50) $('.navbar').addClass('fix') && $('#logo').attr("src", 'images/logo.svg' );
-        else $('.navbar').removeClass('fix') && $('#logo').attr("src", 'images/logo.png' ) ;
-    });
+    if ($('#contact').length > 0) {
+        (function () {
+            $(window).on("scroll", function() {
+                if ($(window).scrollTop() > 50) $('.navbar').addClass('fix') && $('#logo').attr("src", 'images/logo.svg' );
+                else $('.navbar').removeClass('fix') && $('#logo').attr("src", 'images/logo.png' ) ;
+            });
+        })();
+    }
 
 
-    //слайдер с коментариями
-    $('.slide-coment').owlCarousel({
-        loop:true,
-        // margin:10,
-        autoplay: true,
-        nav:true,
-        center: true,
-        smartSpeed:1000, //Время движения слайда
-        autoplayTimeout:6000, //Время смены слайда
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    });
 
-    // слайдер с логотипами
-    $('.slide-partners').owlCarousel({
-        loop:true, //Зацикливаем слайдер
-        // margin:10, //Отступ от картино если выводите больше 1
-        nav:false, //Отключил навигацию
-        autoplay:true, //Автозапуск слайдера
-        smartSpeed:2000, //Время движения слайда
-        autoplayTimeout:4000, //Время смены слайда
-        responsive:{ //Адаптация в зависимости от разрешения экрана
-            0:{
-                items:5
-            },
-            600:{
-                items:5
-            },
-            1000:{
-                items:5
-            }
-        }
-    });
 
-    // слайдер с блогами
-    $('.slide-blog').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:3
-            }
-        }
-    });
+
+   //slider
+    if ($('.owl-carousel').length > 0) {
+        (function () {
+            //слайдер с коментариями
+            $('.slide-coment').owlCarousel({
+                loop:true,
+                // margin:10,
+                autoplay: true,
+                nav:true,
+                center: true,
+                smartSpeed:1000, //Время движения слайда
+                autoplayTimeout:6000, //Время смены слайда
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:1
+                    },
+                    1000:{
+                        items:1
+                    }
+                }
+            });
+            // слайдер с логотипами
+            $('.slide-partners').owlCarousel({
+                loop:true, //Зацикливаем слайдер
+                // margin:10, //Отступ от картино если выводите больше 1
+                nav:false, //Отключил навигацию
+                autoplay:true, //Автозапуск слайдера
+                smartSpeed:2000, //Время движения слайда
+                autoplayTimeout:4000, //Время смены слайда
+                responsive:{ //Адаптация в зависимости от разрешения экрана
+                    0:{
+                        items:5
+                    },
+                    600:{
+                        items:5
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            });
+
+            // слайдер с блогами
+            $('.slide-blog').owlCarousel({
+                loop:true,
+                margin:10,
+                nav:true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:3
+                    },
+                    1000:{
+                        items:3
+                    }
+                }
+            });
+
+        })();
+    }
+
 
 
 
